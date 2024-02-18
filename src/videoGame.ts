9fba121dc4ecbd5601e2135ddc71a2695c6605c2
Seiki Tokunaga <otokuna46@gmail.com>
2024-02-18 20:37:14 +0900

    テストケースの追加・いろいろなクエリについて検証

--- src/videoGame.ts
+++ src/videoGame.ts
@@ -28,10 +28,26 @@ export async function createTable() {
 		AttributeDefinitions: [
 			{ AttributeName: 'Hardware', AttributeType: 'S' },  // 文字列属性
 			{ AttributeName: 'GameId', AttributeType: 'S' },  // 文字列属性
+			{ AttributeName: 'approvalStatus', AttributeType: 'S' },  // 文字列属性
+			{ AttributeName: 'companyName', AttributeType: 'S' },  // 文字列属性
 		],
-		StreamSpecification: {
-			StreamEnabled: false,
-		},
+		GlobalSecondaryIndexes: [
+			{
+				IndexName: 'statusIndex',
+				KeySchema: [
+					{ AttributeName: 'approvalStatus', KeyType: 'HASH' },  // GSI のハッシュキーを指定してください
+					{ AttributeName: 'companyName', KeyType: 'RANGE' }, 
+					// 他のキーも必要に応じて追加してください
+				],
+				Projection: {
+					ProjectionType: 'ALL',  // GSI の投影タイプを指定してください（例: ALL、KEYS_ONLY、INCLUDE）
+				},
+				ProvisionedThroughput: {
+					ReadCapacityUnits: 5,  // GSI の読み取りキャパシティユニットを指定してください
+					WriteCapacityUnits: 5,  // GSI の書き込みキャパシティユニットを指定してください
+				},
+			},
+		],
 	};
 
 	try {
@@ -48,7 +64,7 @@ export async function documentClient_get() {
 		TableName: 'Games',
 		Key: {
 			Hardware: 'SFC',
-			GameId: '1990-SuperMarioWorld'
+			GameId: '1995-SuperMarioWorld'
 		}
 	};
 
@@ -62,61 +78,264 @@ export async function documentClient_get() {
 }
 
 
-export async function documentClient_batchWrite() {
-  const params: AWS.DynamoDB.DocumentClient.BatchWriteItemInput = {
-    RequestItems: {
-      'Games': [
-        {
-          PutRequest: { Item: { Hardware: 'FC', GameId: '1984-lode-runner', Title: 'Lode Runner', Players: 1, Genre: 'ACT' }}
-        },
-        {
-          PutRequest: { Item: { Hardware: 'FC', GameId: '1985-exed-exes', Title: 'Exed Exes', Players: 1, Genre: 'STG' }}
-        },
-        {
-          DeleteRequest: { Key: { Hardware: 'SFC', GameId: '1990-SuperMarioWorld' }}
-        }
-      ]
-    }
-  };
+export async function documentClient_insert() {
+	const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+		TableName: 'Games',
+		Item: {
+			Hardware: 'SFC2',
+			GameId: '1995-SuperMarioWorld',
+			approvalStatus: 'active'
+		},
+		ReturnValues: "ALL_OLD"
+	};
 
-  try {
-    const result = await documentClient.batchWrite(params).promise();
-    console.log(result);
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		const result = await documentClient.put(params).promise();
+		console.log(result);
+		return result
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function insertMockData() {
+	const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+		TableName: 'Games',
+		Item: {
+			Hardware: 'SFC2',
+			GameId: '1995-SuperMarioWorld',
+			approvalStatus: 'inactive',
+			savedTimeStamp: '2024-02-18T09:10:48.717Z'
+		},
+		ReturnValues: "ALL_OLD"
+	};
+
+	const params2: AWS.DynamoDB.DocumentClient.PutItemInput = {
+		TableName: 'Games',
+		Item: {
+			Hardware: 'SFC2',
+			GameId: '1999-SuperMarioWorld',
+			approvalStatus: 'inactive',
+			savedTimeStamp: new Date().toISOString()
+		},
+		ReturnValues: "ALL_OLD"
+	};
+
+	try {
+		const result = await documentClient.put(params).promise();
+		//console.log(result);
+
+		const result2 = await documentClient.put(params2).promise();
+		console.log(result);
+		return result
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function insertMockCopmanyData() {
+	const params: AWS.DynamoDB.DocumentClient.PutItemInput = {
+		TableName: 'Games',
+		Item: {
+			Hardware: 'SFC3',
+			GameId: '2000-SuperMarioWorld',
+			approvalStatus: 'inactive',
+			savedTimeStamp: '2024-02-18T09:10:48.717Z',
+			companyName: '512345'
+		},
+		ReturnValues: "ALL_OLD"
+	};
+
+	const params2: AWS.DynamoDB.DocumentClient.PutItemInput = {
+		TableName: 'Games',
+		Item: {
+			Hardware: 'SFC3',
+			GameId: '2000-SuperMarioWorld',
+			approvalStatus: 'inactive',
+			savedTimeStamp: new Date().toISOString(),
+			companyName: '512345'
+		},
+		ReturnValues: "ALL_OLD"
+	};
+
+	try {
+		const result = await documentClient.put(params).promise();
+		//console.log(result);
+
+		const result2 = await documentClient.put(params2).promise();
+		console.log(result);
+		return result
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function documentClient_query() {
+	const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+		TableName: 'Games',
+		KeyConditionExpression: 'approvalStatus=:gameId',
+		IndexName: 'statusIndex', // 作成したGSI名
+		Limit: 1,
+		ScanIndexForward: false, // true = ascending, false = descending
+		ExpressionAttributeValues: { ':gameId': 'active' }
+	};
+	try {
+		const result = await documentClient.query(params).promise();
+		console.log(result);
+		return result
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function searchWithHashKey(hashkey) {
+	const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+		TableName: 'Games',
+		Limit: 1,
+		KeyConditionExpression: '#hw = :hardware',
+		ExpressionAttributeNames : {
+        "#hw"  : 'Hardware' // GSIの作成時に指定したキー名を設定
+      },
+		ScanIndexForward: false, // true = ascending, false = descending
+		ExpressionAttributeValues: { ':hardware': hashkey}
+	};
+	try {
+		const result = await documentClient.query(params).promise();
+		console.log(result);
+		return result
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function queryInActiveStatus() {
+	const params: AWS.DynamoDB.DocumentClient.QueryInput = {
+		TableName: 'Games',
+		KeyConditionExpression: '#approvalStatus=:gameStatus',
+		ExpressionAttributeNames : {
+        "#approvalStatus"  : 'approvalStatus' // GSIの作成時に指定したキー名を設定
+      },
+		IndexName: 'statusIndex', // 作成したGSI名
+		Limit: 2,
+		ScanIndexForward: false, // true = ascending, false = descending
+		ExpressionAttributeValues: { ':gameStatus': 'inactive' }
+	};
+	try {
+		const result = await documentClient.query(params).promise();
+		console.log(result);
+		return result
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function documentClient_batchWrite() {
+	const params: AWS.DynamoDB.DocumentClient.BatchWriteItemInput = {
+		RequestItems: {
+			'Games': [
+				{
+					PutRequest: { Item: { Hardware: 'FC', GameId: '1984-lode-runner', Title: 'Lode Runner', Players: 1, Genre: 'ACT', SavedTimeStamp: new Date().getTime() } }
+				},
+				{
+					PutRequest: { Item: { Hardware: 'FC', GameId: '1985-exed-exes', Title: 'Exed Exes', Players: 1, Genre: 'STG', SavedTimeStamp: new Date().getTime() } }
+				},
+				{
+					DeleteRequest: { Key: { Hardware: 'SFC', GameId: '1990-SuperMarioWorld' } }
+				}
+			]
+		}
+	};
+
+	try {
+		const result = await documentClient.batchWrite(params).promise();
+		console.log(result);
+	} catch (err) {
+		console.error(err);
+	}
 }
 
 export async function updateItem() {
-  const params: AWS.DynamoDB.UpdateItemInput = {
-    TableName: 'Games',
-    // 更新するアイテムを特定するプライマリーキー属性
-    Key: {
-      Hardware: { S: 'SFC' },
-      GameId: { S: '1990-SuperMarioWorld' }
-    },
-    // 属性値の更新方法を下記 3 プロパティで設定
-    UpdateExpression: 'set Title = :x, #a = :y',
-    ExpressionAttributeNames: {
-      '#a': 'Maker',
-    },
-    ExpressionAttributeValues: {
-      ':x': { S: 'Mario 4' },
-      ':y': { S: 'Nintendo' }
-    },
-    // 更新後の内容を戻り値で知りたいとき（ALL_OLD なら更新前の値が返される）
-    ReturnValues: 'ALL_NEW',
-  };
+	const params: AWS.DynamoDB.UpdateItemInput = {
+		TableName: 'Games',
+		// 更新するアイテムを特定するプライマリーキー属性
+		Key: {
+			Hardware: { S: 'SFC' },
+			GameId: { S: '1990-SuperMarioWorld' }
+		},
+		// 属性値の更新方法を下記 3 プロパティで設定
+		UpdateExpression: 'set Title = :x, #a = :y',
+		ExpressionAttributeNames: {
+			'#a': 'Maker',
+		},
+		ExpressionAttributeValues: {
+			':x': { S: 'Mario 4' },
+			':y': { S: 'Nintendo' }
+		},
+		// 更新後の内容を戻り値で知りたいとき（ALL_OLD なら更新前の値が返される）
+		ReturnValues: 'ALL_NEW',
+	};
 
-  try {
-    const result = await dynamoDb.updateItem(params).promise();
+	try {
+		const result = await dynamoDb.updateItem(params).promise();
+		return result
+		//console.log(result);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+export async function updateItem2() {
+	const params: AWS.DynamoDB.UpdateItemInput = {
+		TableName: 'Games',
+		// 更新するアイテムを特定するプライマリーキー属性
+		Key: {
+			Hardware: { S: 'SFC2' },
+			GameId: { S: '1995-SuperMarioWorld' }
+		},
+		//  Hardware: 'SFC2',
+        //  savedTimeStamp: '2024-02-18T09:10:48.717Z',
+        //  GameId: '1995-SuperMarioWorld'
+		// 属性値の更新方法を下記 3 プロパティで設定
+		UpdateExpression: 'set Title = :x, #a = :y',
+		ExpressionAttributeNames: {
+			'#a': 'approvalStatus',
+		},
+		ExpressionAttributeValues: {
+			':x': { S: 'SuperMario' },
+			':y': { S: 'active' }
+		},
+		// 更新後の内容を戻り値で知りたいとき（ALL_OLD なら更新前の値が返される）
+		ReturnValues: 'ALL_NEW',
+	};
+
+	try {
+		const result = await dynamoDb.updateItem(params).promise();
+		return result
+		//console.log(result);
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+
+export async function updateBatch(){
+	const params = {
+  TableName: 'Games',
+  IndexName: 'statusIndex',
+  KeyConditionExpression: '#approvalStatus = :approvalStatus AND #companyName = :indexValue',
+   ExpressionAttributeNames: {
+	'#companyName': 'companyName',
+	'#approvalStatus': 'approvalStatus'
+  },
+  ExpressionAttributeValues: {
+	':indexValue': {S: '512345'},
+	':approvalStatus': {S: 'active'},
+  },
+};
+	const result = await dynamoDb.query(params).promise()
 	return result
-    //console.log(result);
-  } catch (err) {
-    console.error(err);
-  }
 }
 // 深い比較を行う関数
 export function deepEqual(obj1: any, obj2: any): boolean {
-    return JSON.stringify(obj1) === JSON.stringify(obj2);
+	return JSON.stringify(obj1) === JSON.stringify(obj2);
 }
--- test/documentClient2.test.ts
+++ test/documentClient2.test.ts
@@ -1,7 +1,7 @@
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 import { Movie, Info } from '../src/movie';
-import { createTable, documentClient_get, documentClient_batchWrite, deepEqual, updateItem } from '../src/videoGame';
+import { createTable, documentClient_get, documentClient_batchWrite, deepEqual, updateItem, documentClient_query, documentClient_insert, queryInActiveStatus, insertMockData, updateItem2, insertMockCopmanyData, updateBatch, searchWithHashKey } from '../src/videoGame';
 
 const dynamodb = new DocumentClient({
 	credentials: {
@@ -11,19 +11,43 @@ const dynamodb = new DocumentClient({
 	region: 'ap-northeast-1',
 	endpoint: 'http://localhost:8000',
 });
-
+beforeAll(async () => {
+	await createTable()
+});
 test('write items', async () => {
-	//await createTable()
+	const result = await documentClient_insert()
+	//expect(result.GameId).toBe('1990-SuperMarioWorld')
 })
 
 test('read item', async () => {
-	const result = await documentClient_get()
-	expect(result.Item).toBeUndefined()
+	//	const result = await documentClient_get()
 })
 
+
+test('query item', async () => {
+	await documentClient_batchWrite()
+	const result = await documentClient_query()
+	expect(result.Items.length).toBe(0)
+})
+
+test('query inactive item', async () => {
+	const result = await queryInActiveStatus()
+	expect(result.Items.length).not.toBe(0)
+})
+
+test('query inactive item with batch', async () => {
+	await insertMockData()
+	const result = await queryInActiveStatus()
+	expect(result.Items.length).toBeGreaterThan(1)
+})
 test('update item', async () => {
 	const updatedItem = await updateItem()
 	expect(updatedItem.Attributes.Maker.S).toBe('Nintendo')
+})
+
+test('update item as active', async () => {
+	const updatedItem = await updateItem2()
+	expect(updatedItem.Attributes.approvalStatus.S).toBe('active')
 })
 
 test('delete item', async () => {
@@ -36,5 +60,14 @@ test('batch queue item', async () => {
 	const isEmpty = deepEqual(result, empty)
 	expect(isEmpty).toBeTruthy()
 })
+
+test('batch update item', async () => {
+	await insertMockCopmanyData()
+	await updateBatch()
+	const searchList = await searchWithHashKey('SF3')
+	expect(searchList.Items.length).toBe(0)
+})
+
+
 
 
